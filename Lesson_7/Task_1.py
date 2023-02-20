219d525cd7ce8caf7a98ebe6ff1079d04ae6cf36
BackDad <80159634+BackDad@users.noreply.github.com>
2023-02-20 19:28:30 +0300

    Delete Task_1.py

--- Lesson_7/Task_1.py
+++
@@ -1,9 +0,0 @@
-transformation = lambda x: x
-
-values = [1, 23, 42, "abc"]
-transformed_values = list(map(transformation, values))
-if values == transformed_values:
-    print('ok')
-else:
-    print('fail')
-    
